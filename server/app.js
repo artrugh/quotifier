855b645ae26516d58478fa050d02be60a11261a9
NLJZ <johnson.nathan.lee@gmail.com>
2020-10-19 08:03:28 +0200

    add health check

--- server/app.js
+++ server/app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT;
 
 app.use("/graphql", graphqlHTTP({}));
 
+// Add a health check route in express
+app.get("/_health", (req, res) => {
+  res.status(200).send("ok");
+});
+
 app.listen(PORT, () => {
   console.log(`listening to requests on port ${PORT}`);
 });
