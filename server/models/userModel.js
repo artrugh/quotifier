ca76a6891ed3138073b2c57e1c81c0a796332b94
NLJZ <johnson.nathan.lee@gmail.com>
2020-10-20 17:27:50 +0200

    add userModel

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  userFirst: {
+    type: String,
+    trim: true,
+  },
+  userLast: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: "Email is required,",
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: "Password is required.",
+    trim: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
