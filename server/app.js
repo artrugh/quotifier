bee9d0819868742ba45ffec33ba1bebe6f542339
NLJZ <johnson.nathan.lee@gmail.com>
2020-10-21 10:18:44 +0200

    added code comments

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+// connect database
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_URL, {
