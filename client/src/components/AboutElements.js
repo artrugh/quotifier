42ee9f760889450ea63a907d4be0f1082336b6c3
YamanKatmawi <yamankatmawi@gmx.de>
2020-10-21 13:16:56 +0200

    About branch create

--- client/src/components/AboutElements.js
+++ client/src/components/AboutElements.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const AboutElements = () => {
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <p>text </p>
+    </div>
+  );
+};
+
+export default AboutElements;
