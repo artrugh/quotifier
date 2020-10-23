65c0b89245be1c1f9ef261d02cf9511d1c6ebdf7
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-23 14:12:57 +0200

    nav situation

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
+//--------------components----------------------------
 import AboutElements from "./components/AboutElements";
+import HomeElements from "./components/HomeElements";
 import Help from "./components/Help";
 import Nav from "./components/Nav";
+//----------------Routing--------------------------
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//---------------Aux-------------------------------
+import Aux from "./hoc/Aux";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Quotes and Notes</h1>
-
-      <h1>Notes and Quotes</h1>
-
-      <h1>Quotifier</h1>
-
-      <AboutElements />
-      <Help />
-      <Nav />
-    </div>
+    <Aux>
+      <div className="App">
+        {/* <h1>Quotes and Notes</h1>
+        <h1>Notes and Quotes</h1>
+        <h1>Quotifier</h1> */}
+        {/* <Nav />
+        <Router>
+          <Switch>
+            <Route path="/" exact component={HomeElements} />
+            <Route path="/About" component={AboutElements} />
+            <Route path="/Help" component={Help} />
+          </Switch>
+        </Router> */}
+      </div>
+    </Aux>
   );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,29 @@
 import React from "react";
+//---------Routing-------------------------------
+// import { Link } from "react-router-dom";
+//---------Routing-------------------------------
+import Aux from "../hoc/Aux";
 
-const Nav = () => {
+const Nav = (props) => {
   return (
-    <div className="Nav">
-      <h2>Navbar</h2>
-    </div>
+    <Aux>
+      <div className="nav_container">
+        {/* <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Help">Help</Link>
+          </li>
+          <li>
+            <Link to="/SignIn">Sign In</Link>
+          </li>
+        </ul> */}
+      </div>
+    </Aux>
   );
 };
 
