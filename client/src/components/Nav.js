ba784348cd54ba18ab6007c0d0720a3e6295ef25
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-27 18:08:06 +0100

    fonts added

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,16 +6,16 @@ function Nav() {
   return (
     <div className="nav-container">
       <ul className="nav-links">
-        <li className="nav-link">
+        <li className="nav-link home-link">
           <Link to="/Home">Home</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link about-link">
           <Link to="/About">About</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link help-link">
           <Link to="/Help">Help</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link sign-in-link">
           <Link to="/SignIn">Sign In</Link>
         </li>
       </ul>
