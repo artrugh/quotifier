2b8f1227bd4a58e11ca8228c4bbac549aa4d931f
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-28 14:51:33 +0100

    nav items and reset amend

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -9,12 +9,12 @@ function Nav() {
         <li className="nav-link home-link">
           <Link to="/Home">Home</Link>
         </li>
-        {/* <li className="nav-link about-link">
+        <li className="nav-link about-link">
           <Link to="/About">About</Link>
         </li>
         <li className="nav-link help-link">
           <Link to="/Help">Help</Link>
-        </li> */}
+        </li>
         <li className="nav-link sign-in-link">
           <Link to="/SignIn">Sign in</Link>
         </li>
