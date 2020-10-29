2f6c27ca1d6c0f81d3320dc35ffd5cb2f96c69cc
NLJZ <johnson.nathan.lee@gmail.com>
2020-10-29 16:25:27 +0100

    added user login form. with correct data the browser now recieves JWT

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,17 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+const axios = require("axios");
 
 const LoginForm = () => {
-  const [loginData, setLoginData] = useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(null);
+
+  const options = {
+    url: "/api/v1/users/login",
+    mode: "cors",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: { email: email, password: password },
+  };
+
+  const submitForm = () => {
+    axios(options)
+      .then((response) => {
+        console.log(response);
+        setRedirect(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response);
+      });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     submitForm();
   };
 
+  if (redirect) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <form onSubmit={handleSubmit}>
-      <input type="email" className="input" placeholder="email" />
-      <input type="password" className="input" placeholder="password" />
+      <input
+        type="email"
+        className="input"
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+        required
+      />
+      <input
+        type="password"
+        className="input"
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+        required
+      />
       <button type="submit" value="Submit">
         Submit
       </button>
