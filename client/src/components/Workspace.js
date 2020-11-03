c64d817372ca448022b8af2bebfe2c628f4f9049
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-03 15:58:21 +0100

    displays username from redux state in workspace

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -3,12 +3,10 @@ import LogOutButton from "./LogOutButton";
 import { useSelector, useDispatch } from "react-redux";
 
 const Workspace = () => {
-  const sources = useSelector((state) => state.sources);
-  console.log(sources);
-  const userData = useSelector((state) => state.userData);
+  const userName = useSelector((state) => state.userName);
   return (
     <div className="workspace">
-      You are logged in.
+      <h2>Hello {userName}.</h2>
       <p className="text">
         here you can implement all your quotes and find them every time you need
         them just by clicking on the tags{" "}
--- client/src/redux/reducers/userData.js
+++ client/src/redux/reducers/userData.js
@@ -3,8 +3,6 @@ const userReducer = (state = null, action) => {
     case "USER_NAME":
       let userName = action.payload.userFirst + " " + action.payload.userLast;
       return userName;
-    case "LOADING":
-      return { ...state, loading: true };
     default:
       return state;
   }
