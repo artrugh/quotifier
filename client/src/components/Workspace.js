59c018f6b21e59f9cd8c55786598ac7d04715dd7
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-11-05 14:01:24 +0100

    preliminarry layout settings workspace

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -3,11 +3,8 @@ import LogOutButton from "./LogOutButton";
 import WorkspaceMenuTop from "./WorkspaceMenuTop";
 import WorkspaceMenuLeft from "./WorkspaceMenuLeft";
 import WorkspaceMain from "./WorkspaceMain";
-// import { useSelector, useDispatch } from "react-redux";
 
 const Workspace = () => {
-  // const userName = useSelector((state) => state.userName);
-
   return (
     <div className="workspace">
       <div className="workspace-left">
@@ -15,11 +12,8 @@ const Workspace = () => {
       </div>
 
       <div className="workspace-right">
-        {" "}
         <WorkspaceMenuTop />
         <WorkspaceMain />
-        {/* <h2>Hello {userName}.</h2> */}
-        {/* <LogOutButton /> */}
       </div>
     </div>
   );
