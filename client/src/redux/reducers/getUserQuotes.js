ea15b1dc289bed3c38cfedc26ff103de47a5ceb4
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-07 13:14:23 +0100

    bug check

--- client/src/redux/reducers/getUserQuotes.js
+++ client/src/redux/reducers/getUserQuotes.js
@@ -1,6 +1,17 @@
-const quotesReducer = (state = [], action) => {
+// const convertArrayToObject = (array, key) => {
+//   const initialValue = {};
+//   return array.reduce((obj, item) => {
+//     return {
+//       ...obj,
+//       [item[key]]: item,
+//     };
+//   }, initialValue);
+// };
+
+const quotesReducer = (state = {}, action) => {
   switch (action.type) {
     case "LOAD_QUOTES":
+      // let newState = convertArrayToObject(action.payload, "id");
       return action.payload;
     case "RESET":
       return;
