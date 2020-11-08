399f17316232eca98fafe8dad41ae472699ab20a
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-08 08:59:25 +0100

    added Helper functions to get one quote or source

--- client/src/helpers/getUserData.js
+++ client/src/helpers/getUserData.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 
+// returns all User Sources
+
 export const getSources = async () => {
   const sourceOptions = {
     url: "/api/v1/data/getSources",
@@ -14,9 +16,43 @@ export const getSources = async () => {
   return data;
 };
 
+// returns all User Quotes
+
 export const getQuotes = async () => {
   const quoteOptions = {
     url: "/api/v1/data/getQuotes",
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await axios(quoteOptions);
+  const data = await response.data;
+  return data;
+};
+
+// returns one User Quote with ID
+
+export const getOneQuote = async (quoteId) => {
+  const quoteOptions = {
+    url: `/api/v1/data/getQuote/${quoteId}`,
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await axios(quoteOptions);
+  const data = await response.data;
+  return data;
+};
+
+// returns one User Source with ID
+
+export const getOneSource = async (sourceId) => {
+  const sourceOptions = {
+    url: `/api/v1/data/getSource/${sourceId}`,
     mode: "cors",
     method: "GET",
     headers: {
