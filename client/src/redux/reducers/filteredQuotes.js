a4e045365d4b588710f704247e9ae57e0bcbc402
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-11 09:14:49 +0100

    all quotes returns new array, saved immer example

--- client/src/redux/reducers/filteredQuotes.js
+++ client/src/redux/reducers/filteredQuotes.js
@@ -2,12 +2,14 @@ import produce from "immer";
 
 const quotesReducer = (state = [], action) => {
   switch (action.type) {
+    // case "SHOW_ALL_QUOTES":
+    //   const baseState = state;
+    //   const newArr = action.payload;
+    //   const allQuotesArr = produce(baseState, (draft) => {
+    //     newArr.forEach((quote) => draft.push(quote));
+    //   });
     case "SHOW_ALL_QUOTES":
-      const baseState = state;
-      const newArr = action.payload;
-      const allQuotesArr = produce(baseState, (draft) => {
-        newArr.forEach((quote) => draft.push(quote));
-      });
+      const allQuotesArr = action.payload;
       return allQuotesArr;
     default:
       return state;
