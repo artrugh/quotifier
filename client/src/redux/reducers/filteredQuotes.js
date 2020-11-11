4a6fa18c0a4185b75dce66e3e78288c7423fdc93
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-11 10:31:04 +0100

    basic filtered quotes setup completed

--- client/src/redux/reducers/filteredQuotes.js
+++ client/src/redux/reducers/filteredQuotes.js
@@ -2,12 +2,12 @@ import produce from "immer";
 
 const quotesReducer = (state = [], action) => {
   switch (action.type) {
-    // case "SHOW_ALL_QUOTES":
-    //   const baseState = state;
-    //   const newArr = action.payload;
-    //   const allQuotesArr = produce(baseState, (draft) => {
-    //     newArr.forEach((quote) => draft.push(quote));
-    //   });
+    case "SHOW_ALL_QUOTES":
+    // const baseState = state;
+    // const newArr = action.payload;
+    // const allQuotesArr = produce(baseState, (draft) => {
+    //   newArr.forEach((quoteID) => draft.push(quoteID));
+    // });
     case "SHOW_ALL_QUOTES":
       const allQuotesArr = action.payload;
       return allQuotesArr;
