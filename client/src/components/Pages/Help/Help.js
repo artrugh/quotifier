23a6c1c04ab9a323afa24bff10be5d5af2cecbd3
YamanKatmawi <yamankatmawi@gmx.de>
2020-11-11 11:03:50 +0100

    LoadingAnimation added

--- client/src/components/Pages/Help/Help.js
+++ client/src/components/Pages/Help/Help.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LoadingAnimation from "../../Animation/LoadingAnimation";
 
 function help() {
   return (
     <div className="help">
       <h2>help</h2>
+      <LoadingAnimation />
     </div>
   );
 }
