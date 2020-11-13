76c8f40afe15e1c59f11372fbc9d0ecb86796e9b
arturo <42832783+artrugh@users.noreply.github.com>
2020-11-13 23:55:22 +0100

    Update Greeting.js

--- client/src/components/Workspace/Greeting.js
+++ client/src/components/Workspace/Greeting.js
@@ -4,13 +4,13 @@ import { useSelector } from "react-redux";
 const Greeting = () => {
   const userName = useSelector((state) => state.userName);
   return (
-    <React.Fragment>
+    <>
       <h2>Hello {userName}.</h2>
       <p className="text">
         here you can implement all your quotes and find them every time you need
         them just by clicking on the tags{" "}
       </p>
-    </React.Fragment>
+    </>
   );
 };
 
