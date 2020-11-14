cf74c493460d261b786801ed483ac90f125dbc50
arturo <42832783+artrugh@users.noreply.github.com>
2020-11-14 09:23:23 +0100

    Update getUserQuotes.js

--- client/src/redux/reducers/getUserQuotes.js
+++ client/src/redux/reducers/getUserQuotes.js
@@ -3,7 +3,7 @@ import { arrayToObject } from "../../helpers/arrayToObject.js";
 const quotesReducer = (state = {}, action) => {
   switch (action.type) {
     case "LOAD_QUOTES":
-      let newState = arrayToObject(action.payload, "_id");
+      const newState = arrayToObject(action.payload, "_id");
       return newState;
     default:
       return state;
